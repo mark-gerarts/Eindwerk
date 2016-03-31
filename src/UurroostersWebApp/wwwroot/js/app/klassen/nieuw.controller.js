2281774649617cc562de3b5d41b0ca3cf4e3ca32
mark-gerarts <mark.gerarts@gmail.com>
2016-03-31 09:47:26 +0200

    ~finished klassen

--- src/UurroostersWebApp/wwwroot/js/app/klassen/nieuw.controller.js
+++ src/UurroostersWebApp/wwwroot/js/app/klassen/nieuw.controller.js
@@ -3,7 +3,7 @@
 
     angular.module("klassenApp").controller("nieuweKlasCtrl", nieuweKlasCtrl);
 
-    function nieuweKlasCtrl(klassenService) {
+    function nieuweKlasCtrl(klassenService, campussenService, studierichtingenService) {
         var vm = this;
 
         vm.isBusy = false;
@@ -11,7 +11,22 @@
         vm.errorMessage = "";
         vm.isSubmittedOnce = false;
 
+        vm.campussen = [];
+        vm.studierichtingen = [];
+
         vm.nieuweKlas = {};
+
+        campussenService.async().then(function (r) {
+            vm.campussen = campussenService.getAll();
+        }, function () {
+            //
+        });
+
+        studierichtingenService.async().then(function (r) {
+            vm.studierichtingen = studierichtingenService.getAll();
+        }, function () {
+            //
+        });
 
         vm.insertKlas = function () {
             vm.isBusy = true;
