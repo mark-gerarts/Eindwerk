12d421c95695b878ade245a84b88438d586f8ae1
Mark <mark.gerarts@gmail.com>
2016-04-04 10:32:55 +0200

    begin lessen

---
+++ src/UurroostersWebApp/Models/Les.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UurroostersWebApp.Models
+{
+    public class Les
+    {
+        public int Id { get; set; }
+
+        public int Jaar { get; set; }
+
+        public Lesblok Lesblok { get; set; }
+
+        public Leerkracht Leerkracht { get; set; }
+
+        public Lokaal Lokaal { get; set; }
+
+        public Klas Klas { get; set; }
+    }
+}
---
+++ src/UurroostersWebApp/Models/Lesblok.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UurroostersWebApp.Models
+{
+    public class Lesblok
+    {
+        public int Id { get; set; }
+
+        public TimeSpan Starttijd { get; set; }
+
+        public TimeSpan Eindtijd { get; set; }
+
+        public Dag Dag { get; set; }
+    }
+}
