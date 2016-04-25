90c87cfd67ac32c2e2ee9d38a0ef067fb81d12c8
mark-gerarts <mark.gerarts@gmail.com>
2016-04-25 20:38:42 +0200

    start on add nieuw event

--- src/UurroostersWebApp/wwwroot/js/app/events/nieuw.controller.js
+++ src/UurroostersWebApp/wwwroot/js/app/events/nieuw.controller.js
@@ -3,9 +3,39 @@
 
     angular.module("eventsApp").controller("eventsNieuwCtrl", eventsNieuwCtrl);
 
-    function eventsNieuwCtrl() {
+    function eventsNieuwCtrl($q, klassenService, campussenService) {
         var vm = this;
+        
+        vm.isLoading = true;
+        vm.klassen = [];
 
-        console.log('showtime')
+        vm.init = function () {
+            vm.isLoading = true;
+
+            function getData(service) {
+                var defer = $q.defer();
+                service.async().then(function (r) {
+                    defer.resolve(service.getAll());
+                });
+                return defer.promise;
+            }
+            $q.all([
+                getData(klassenService),
+                //getData(lesblokkenService),
+                //getData(leerkrachtenService),
+                //getData(vakkenService),
+                //getData(dagenService),
+                getData(campussenService)
+            ]).then(function (data) {
+                angular.copy(data[0], vm.klassen);
+                angular.copy(data[1], vm.campussen);
+
+                //vm.currentKlas = klassenService.findById($routeParams.klasID);
+                //vm.initialiseNieuweLes();
+
+                vm.isLoading = false;
+            })
+        }
+        vm.init();
     }
-})
+})();
